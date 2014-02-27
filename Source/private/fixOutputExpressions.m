71ece7310072d96e9097e9769673cef5117fb0f0
David Hagen <drhagen@mit.edu>
2014-02-27 07:31:36 -0500

    Typo in fixOutputExpressions

--- Source/private/fixOutputExpressions.m
+++ Source/private/fixOutputExpressions.m
@@ -10,5 +10,5 @@ elseif iscellstr(expressions)
     expressions = [vec(expressions), num2cell(ones(numel(expressions),1))];
 else
     assert(size(expressions,2) == 2 && isnumeric([expressions{:,2}]), ...
-        'KroneckerBio:Output:InvalidContributors', 'Output has an invalid listof contributors')
+        'KroneckerBio:Output:InvalidContributors', 'Output has an invalid list of contributors')
 end
