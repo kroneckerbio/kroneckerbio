442a09de5727af0d9577752ea665ce9c5c2962eb
David Hagen <david@drhagen.com>
2015-11-06 18:54:36 -0500

    Updated documentation for LoadModel*Analytic Closes #69

--- Source/LoadModelSbmlAnalytic.m
+++ Source/LoadModelSbmlAnalytic.m
@@ -1,32 +1,43 @@
-function m = LoadModelSbmlAnalytic(filename, opts)
-%LoadModelSbmlAnalytic Import SBML model and covert to kroneckerbio analytic
-%model. Modify the model and add outputs after calling this.
+function m = LoadModelSbmlAnalytic(sbml, opts)
+%LoadModelSbmlAnalytic Import SBML model and covert to Kronecker analytic
+%   model. The model may be further modified by the user.
 %
-%   m = LoadModelSbmlAnalytic(model, yNames, yMembers, yValues, opts)
+%   m = LoadModelSbmlAnalytic(sbml, opts)
 %
 %   Inputs
-%   filename: [ string ]
-%       Path to SBML file. Usually has .sbml or .xml extension
+%   sbml: [ libSBML model object | string ]
+%       LibSBML Model object or name of SBML file to import
 %   opts: [ options struct scalar {} ]
 %       .Verbose [ logical scalar {false} ]
 %       	Print progress to command window
 %       .Validate [ logical scalar {false} ]
 %           Whether to use libSBML's model validation tool
-%       .UseNames [logical scalar {false} ]
-%           Whether to convert SBML IDs to Names and autogenerate new IDs
-%           Use this when the supplied SBML model uses "nice" names as IDs
 %
 %   Outputs
 %   m: [ Model.Analytic struct ]
 %       An analytic kroneckerbio model
 %
-%   Notes: 
-%   - Inputs are any species that have "constant" or "boundaryCondition" set.
-%   - Seeds are generated from any parameter that appears in an InitialAssignment.
+%   Notes
+%   - All species are tracked in amount. Concentration species are
+%   converted with best effort.
+%   - Species that are set to constant or boundaryCondition are
+%   converted to inputs
+%   - Initial assignment rules of species are copied to the initial
+%   condition expression.
+%   - Repeated assignment rules of compartments are copied to the
+%   compartment size.
+%   - Repeated assignment rules of parameters and species get converted
+%   into Kronecker rules.
+%   - Rate rules are converted to reactions with a single product.
+%   - The ID of a component is used if the component has no name.
 %
-%   Limitations:
-%   Not all Simbiology features are compatible with this converter. This
-%   function ignores events, some rules, and all functions in the model.
+%   Limitations
+%   - Not all Simbiology features are compatible with this converter. This
+%   function ignores events, algebraic rules, and all functions in the
+%   model.
+
+% (c) 2015 Kevin Shi and David R Hagen
+% This work is released under the MIT license.
 
 %% Clean up inputs
 if nargin < 2
@@ -36,7 +47,6 @@ end
 % Default options
 opts_.Verbose = 0;
 opts_.Validate = false;
-opts_.UseNames = false;
 
 opts = mergestruct(opts_, opts);
 
@@ -44,19 +54,14 @@ verbose = logical(opts.Verbose);
 opts.Verbose = max(opts.Verbose-1,0);
 
 %% Call libSBML to import SBML model
-if verbose; fprintf('Convert SBML model using libSBML...'); end
+if verbose; fprintf('Load SBML model using libSBML...'); end
 
-sbml = TranslateSBML(filename, double(opts.Validate), opts.Verbose);
+if ischar(sbml)
+    sbml = TranslateSBML(sbml, double(opts.Validate), opts.Verbose);
+end
 
 if verbose; fprintf('done.\n'); end
 
 m = sbml2analytic(sbml);
-
-%% Convert model
-% symbolic = sbml2symbolic(sbml, opts);
-% 
-% assert(isValidSymbolicModel(symbolic), 'LoadModelSbmlAnalytic:InvalidSymbolicModel', 'Symbolic model intermediate failed validation check')
-%     
-% m = symbolic2analytic(symbolic, opts);
 
 end
--- Source/LoadModelSimBioAnalytic.m
+++ Source/LoadModelSimBioAnalytic.m
@@ -1,12 +1,37 @@
 function m = LoadModelSimBioAnalytic(simbio)
-%LoadModelSimBioAnalytic Load analytic model from a Matlab SimBiology model
-%   Modify the model and add outputs after calling this.
+%LoadModelSimBioAnalytic Import Matlab Simbiology model and covert to
+%   Kronecker analytic model. The model may be further modified by the
+%   user.
 %
-%   m = LoadModelSimBioAnalytic(SimbioModel)
+%   m = LoadModelSimBioAnalytic(simbio)
 %
 %   Inputs
 %   simbio: [ simbio model object | string ]
-%       Matlab SimBiology Model object or name of SBML file ot import
+%       Matlab SimBiology Model object or name of SBML file to import
+%
+%   Outputs
+%   m: [ Model.Analytic struct ]
+%       An analytic kroneckerbio model
+%
+%   Notes
+%   - All species are tracked in amount.
+%   - Species that are set to constant or boundaryCondition are
+%   converted to inputs
+%   - Initial assignment rules of species are copied to the initial
+%   condition expression.
+%   - Repeated assignment rules of compartments are copied to the
+%   compartment size.
+%   - Repeated assignment rules of parameters and species get converted
+%   into Kronecker rules.
+%   - Rate rules are converted to reactions with a single product.
+%
+%   Limitations
+%   - Not all Simbiology features are compatible with this converter. This
+%   function ignores events, algebraic rules, and all functions in the
+%   model.
+%   - Simbiology does not expose the dimension of a compartment. All
+%   compartments are given a dimension of 3. This does not affect the
+%   behavior of the model.
 
 % (c) 2015 Kevin Shi, David R Hagen & Bruce Tidor
 % This work is released under the MIT license.
