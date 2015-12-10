3b46a85fd36c39bd27c0109bb491e6525934be9f
David Hagen <david@drhagen.com>
2015-12-10 18:32:19 -0500

    Fixed Update. Cleared more symbolic variables in finalizeModelAnalytic.
    Symbolic variables in the don't save well and are not needed to run the model, so they are cleared before the function handles are constructed.

--- Source/Update.m
+++ Source/Update.m
@@ -5,7 +5,7 @@ if any(regexp(obj.Type, '^Model'))
     % Update a model
     if nargin <= 1
         for i = 1:numel(obj)
-            obj(i) = obj.Update(obj(i).k, obj(i).s, obj(i).q);
+            obj(i) = obj.Update(obj(i).k);
         end
     elseif nargin <= 2
         if ~iscell(varargin{1})
@@ -13,44 +13,18 @@ if any(regexp(obj.Type, '^Model'))
             varargin{1} = varargin(1);
         end
         for i = 1:numel(obj)
-            obj(i) = obj.Update(varargin{1}{i}, obj(i).s, obj(i).q);
-        end
-    elseif nargin <= 3
-        if ~iscell(varargin{1})
-            % Encapsulate
-            varargin{1} = varargin(1);
-        end
-        if ~iscell(varargin{2})
-            % Encapsulate
-            varargin{2} = varargin(2);
-        end
-        for i = 1:numel(obj)
-            obj(i) = obj.Update(varargin{1}{i}, varargin{2}{i}, obj(i).q);
-        end
-    elseif nargin <= 4
-        if ~iscell(varargin{1})
-            % Encapsulate
-            varargin{1} = varargin(1);
-        end
-        if ~iscell(varargin{2})
-            % Encapsulate
-            varargin{2} = varargin(2);
-        end
-        if ~iscell(varargin{3})
-            % Encapsulate
-            varargin{3} = varargin(3);
-        end
-        for i = 1:numel(obj)
-            obj(i) = obj.Update(varargin{1}{i}, varargin{2}{i}, varargin{3}{i});
+            obj(i) = obj.Update(varargin{1}{i});
         end
     end
 elseif any(regexp(obj.Type, '^Experiment'))
     if nargin <= 1
-        obj = obj.Update(obj.s, obj.q);
+        obj = obj.Update(obj.s, obj.q, obj.h);
     elseif nargin <= 2
-        obj = obj.Update(varargin{1}, obj.q);
+        obj = obj.Update(varargin{1}, obj.q, obj.h);
     elseif nargin <= 3
-        obj = obj.Update(varargin{1}, varargin{2});
+        obj = obj.Update(varargin{1}, varargin{2}, obj.h);
+    elseif nargin <= 4
+        obj = obj.Update(varargin{1}, varargin{2}, varargin{3});
     end
 else
     error('KroneckerBio:Update', 'That object type cannot be updated with Update at this time.')
